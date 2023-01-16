3f8444080567c15ea2e11c7ea3a27f4aa9846c0b
Sirapop Na Ranong <n.r.sirapop@gmail.com>
2023-01-16 16:42:41 +0700

    feat: update meta tags, update og image, update services config, use next image

--- config/services.js
+++ config/services.js
@@ -6,13 +6,13 @@ export default (
       path: '/interchain-token',
       others_paths:
         [],
-      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+      description: 'Easily deploy your own cross-chain-enabled ERC-20 token.',
       image: '/images/ogimage.png',
       tags:
         [
-          'Tag 1',
-          'Tag 2',
-          'Tag 3',
+          'Crosschain Token',
+          'Token Linker',
+          'Cross Chain',
         ],
       navbar_visible: true,
       coming_soon: true,
@@ -23,13 +23,13 @@ export default (
       path: '/gmp-express',
       others_paths:
         [],
-      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.',
+      description: `Axelar's GMP Express is our fastest GMP offering. Full end-to-end GMP messages can sometimes take many minutes because they must wait for full finality and network resolution, but with GMP Express, you can take advantage of faster message delivery with integrated support for Axelar token bridging.`,
       image: '/images/ogimage.png',
       tags:
         [
-          'Tag 1',
-          'Tag 2',
-          'Tag 3',
+          'GMP',
+          'GMP Express',
+          'Cross Chain',
         ],
       navbar_visible: true,
       coming_soon: true,

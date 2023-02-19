80d8068684bd2190636712294c42d26c79745579
Alan Soares <alanrsoars@gmail.com>
2023-02-20 11:37:09 +1300

    chore: fix tscheck

--- prettier.config.js
+++ prettier.config.js
@@ -3,6 +3,7 @@
 /** @type {import("@ianvs/prettier-plugin-sort-imports").PrettierConfig} */
 module.exports = {
   plugins: [
+    // @ts-ignore
     require("prettier-plugin-tailwindcss"),
     require("@ianvs/prettier-plugin-sort-imports"),
   ],

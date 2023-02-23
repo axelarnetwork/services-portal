b501176d6cea8554d94b201d371de0fdadccaba9
Alan Soares <alanrsoars@gmail.com>
2023-02-23 16:28:10 +1300

    refactor: add typescript to wallet reducer

--- src/reducers/wallet.ts
+++ src/reducers/wallet.ts
@@ -1,22 +1,39 @@
+import {
+  JsonRpcSigner,
+  Web3Provider,
+  Provider,
+} from "@ethersproject/providers";
 import { createSlice } from "@reduxjs/toolkit";
 
 import { WALLET_DATA, WALLET_RESET } from "./types";
 
-const INITIAL_STATE = {
-  default_chain_id: null,
-  chain_id: null,
-  provider: null,
-  web3_provider: null,
-  signer: null,
-  address: null,
+export type WalletData = {
+  default_chain_id: number | null;
+  chain_id: number | null;
+  provider: Provider | null;
+  web3_provider: Web3Provider | null;
+  signer: JsonRpcSigner | null;
+  address: string | null;
 };
 
-export const wallet = (
-  state = {
-    [WALLET_DATA]: INITIAL_STATE,
-  },
-  action
-) => {
+const INITIAL_STATE = {
+  [WALLET_DATA]: {
+    default_chain_id: null,
+    chain_id: null,
+    provider: null,
+    web3_provider: null,
+    signer: null,
+    address: null,
+  } as WalletData,
+};
+
+export type WalletState = typeof INITIAL_STATE;
+
+type Action =
+  | { type: typeof WALLET_DATA; value: typeof INITIAL_STATE }
+  | { type: typeof WALLET_RESET };
+
+export const wallet = (state = INITIAL_STATE, action: Action) => {
   switch (action.type) {
     case WALLET_DATA:
       return {

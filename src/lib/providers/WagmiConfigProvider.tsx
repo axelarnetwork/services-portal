31cbf11416e781f24a6050150de38b90d7dc631d
Alan Soares <alanrsoars@gmail.com>
2023-02-24 11:17:23 +1300

    chore: remove log

--- src/lib/providers/WagmiConfigProvider.tsx
+++ src/lib/providers/WagmiConfigProvider.tsx
@@ -16,36 +16,29 @@ import {
 import { InjectedConnector } from "wagmi/connectors/injected";
 import { publicProvider } from "wagmi/providers/public";
 
-// const chainConfigs = [
-//   mainnet,
-//   goerli,
-//   moonbeam,
-//   avalanche,
-//   avalancheFuji,
-//   polygon,
-//   polygonMumbai,
-//   bsc,
-//   bscTestnet,
-//   arbitrum,
-//   arbitrumGoerli,
-// ];
 const chainConfigs = [
-  {...mainnet, networkNameOnAxelar: "ethereum", environment: "mainnet"},
-  {...goerli, networkNameOnAxelar: "ethereum-2", environment: "testnet"},
-  {...moonbeam, networkNameOnAxelar: "moonbeam", environment: "testnet"},
-  {...avalanche, networkNameOnAxelar: "avalanche", environment: "mainnet"},
-  {...avalancheFuji, networkNameOnAxelar: "avalanche", environment: "testnet"},
-  {...polygon, networkNameOnAxelar: "polygon", environment: "mainnet"},
-  {...polygonMumbai, networkNameOnAxelar: "polygon", environment: "testnet"},
-  {...bsc, networkNameOnAxelar: "binance", environment: "mainnet"},
-  {...bscTestnet, networkNameOnAxelar: "binance", environment: "testnet"},
-  {...arbitrum, networkNameOnAxelar: "arbitrum", environment: "mainnet"},
-  {...arbitrumGoerli, networkNameOnAxelar: "arbitrum", environment: "testnet"},
+  { ...mainnet, networkNameOnAxelar: "ethereum", environment: "mainnet" },
+  { ...goerli, networkNameOnAxelar: "ethereum-2", environment: "testnet" },
+  { ...moonbeam, networkNameOnAxelar: "moonbeam", environment: "testnet" },
+  { ...avalanche, networkNameOnAxelar: "avalanche", environment: "mainnet" },
+  {
+    ...avalancheFuji,
+    networkNameOnAxelar: "avalanche",
+    environment: "testnet",
+  },
+  { ...polygon, networkNameOnAxelar: "polygon", environment: "mainnet" },
+  { ...polygonMumbai, networkNameOnAxelar: "polygon", environment: "testnet" },
+  { ...bsc, networkNameOnAxelar: "binance", environment: "mainnet" },
+  { ...bscTestnet, networkNameOnAxelar: "binance", environment: "testnet" },
+  { ...arbitrum, networkNameOnAxelar: "arbitrum", environment: "mainnet" },
+  {
+    ...arbitrumGoerli,
+    networkNameOnAxelar: "arbitrum",
+    environment: "testnet",
+  },
 ];
 
 export const getWagmiChains = () => chainConfigs;
-
-console.log("WagmiConfigProvider chainConfigs",chainConfigs);
 
 const { webSocketProvider, provider } = configureChains(chainConfigs, [
   publicProvider(),

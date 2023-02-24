a4aaca8204d673b7081c9a4c6901d7ef1d624a9a
Canh Trinh <canhtrinh@users.noreply.github.com>
2023-02-24 11:31:29 -0500

    chore: add celo and kava wagmi chains (#42)
    * chore: add celo and kava wagmi chains
    
    * chore: small format issue
    
    * chore: adding aurora and optimism
    
    * chore: fixing moonbeam testnet
    
    * chore: adding optimism testnet to wallet config

--- src/lib/providers/WagmiConfigProvider.tsx
+++ src/lib/providers/WagmiConfigProvider.tsx
@@ -12,6 +12,13 @@ import {
   bscTestnet,
   arbitrum,
   arbitrumGoerli,
+  celo,
+  celoAlfajores,
+  aurora,
+  auroraTestnet,
+  optimism,
+  optimismGoerli,
+  moonbaseAlpha,
 } from "wagmi/chains";
 import { InjectedConnector } from "wagmi/connectors/injected";
 import { publicProvider } from "wagmi/providers/public";
@@ -19,7 +26,8 @@ import { publicProvider } from "wagmi/providers/public";
 const chainConfigs = [
   { ...mainnet, networkNameOnAxelar: "ethereum", environment: "mainnet" },
   { ...goerli, networkNameOnAxelar: "ethereum-2", environment: "testnet" },
-  { ...moonbeam, networkNameOnAxelar: "moonbeam", environment: "testnet" },
+  { ...moonbeam, networkNameOnAxelar: "moonbeam", environment: "mainnet" },
+  { ...moonbaseAlpha, networkNameOnAxelar: "moonbeam", environment: "testnet" },
   { ...avalanche, networkNameOnAxelar: "avalanche", environment: "mainnet" },
   {
     ...avalancheFuji,
@@ -35,6 +43,62 @@ const chainConfigs = [
     ...arbitrumGoerli,
     networkNameOnAxelar: "arbitrum",
     environment: "testnet",
+  },
+  { ...celo, networkNameOnAxelar: "celo", environment: "mainnet" },
+  { ...celoAlfajores, networkNameOnAxelar: "celo", environment: "testnet" },
+  { ...aurora, networkNameOnAxelar: "aurora", environment: "mainnet" },
+  { ...auroraTestnet, networkNameOnAxelar: "aurora", environment: "testnet" },
+  { ...optimism, networkNameOnAxelar: "optimism", environment: "mainnet" },
+  {
+    ...optimismGoerli,
+    networkNameOnAxelar: "optimism",
+    environment: "testnet",
+  },
+  {
+    id: 2222,
+    name: "Kava EVM",
+    network: "kava",
+    networkNameOnAxelar: "kava",
+    environment: "mainnet",
+    nativeCurrency: {
+      name: "KAVA",
+      symbol: "KAVA",
+      decimals: 18,
+    },
+    blockExplorers: {
+      default: {
+        name: "Kava Explorer",
+        url: "https://explorer.kava.io/",
+      },
+    },
+    rpcUrls: {
+      default: { http: ["https://evm.kava.io"] },
+      public: { http: ["https://evm.kava.io"] },
+    },
+    testnet: false,
+  },
+  {
+    id: 2221,
+    name: "Kava EVM Testnet",
+    network: "kava",
+    networkNameOnAxelar: "kava",
+    environment: "testnet",
+    nativeCurrency: {
+      name: "KAVA",
+      symbol: "KAVA",
+      decimals: 18,
+    },
+    blockExplorers: {
+      default: {
+        name: "Kava EVM Explorer",
+        url: "https://explorer.evm-alpha.kava.io/",
+      },
+    },
+    rpcUrls: {
+      default: { http: ["https://evm.testnet.kava.io"] },
+      public: { http: ["https://evm.testnet.kava.io"] },
+    },
+    testnet: true,
   },
 ];
 
